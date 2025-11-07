dc743ee6fd5d6644c87c5cb1fa5690b6ba4b5ba5
Lucian Cernauteanu <lucian.cernauteanu@thinslices.com>
2025-11-07 10:00:21 +0200

    chore: format Dart files

--- integration_test/auth/loginToApp.dart
+++ integration_test/auth/loginToApp.dart
@@ -49,7 +49,8 @@ class LoginToApp {
 
     // Handle permission dialog if it appears
     if (await $.native.isPermissionDialogVisible()) {
-      await $.native.grantPermissionOnlyThisTime();
+      await $.native.tap(Selector(text: 'Allow'), appId: 'com.thinslices.solarisdemo');
+      // await $.native.grantPermissionWhenInUse();
     }
     await $.pumpAndSettle(timeout: Duration(seconds: 3));
     // Wait for OTP screen to appear
